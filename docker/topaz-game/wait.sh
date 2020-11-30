d6944b56792cf572e8e9ae1730176156e67955a4
Bitvalues <bitvalues@gmail.com>
2020-11-30 02:07:55 -0500

    Added stuff for connect, game & search

---
+++ docker/topaz-game/wait.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while ! mysql --host=$MYSQL_DB_HOST --port=$MYSQL_DB_PORT --user=$MYSQL_DB_USER --password=$MYSQL_DB_PASSWORD -e "USE $MYSQL_DB_NAME; SELECT 1 FROM zone_weather LIMIT 1"; do
+    sleep 5
+    echo "Retrying connection to Database..."
+done
+
+sleep 5
+echo "Starting topaz game..."
+./topaz_game
