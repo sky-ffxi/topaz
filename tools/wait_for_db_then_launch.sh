d4e29e8cccce9d44d03ed8ec412bd7fe6066daeb
Bitvalues <bitvalues@gmail.com>
2020-11-29 21:08:51 -0500

    Removed navmeshes submodule

--- tools/wait_for_db_then_launch.sh
+++ tools/wait_for_db_then_launch.sh
@@ -2,7 +2,9 @@
 
 while ! mysql --host=$TPZ_DB_HOST --port=$TPZ_DB_PORT --user=$TPZ_DB_USER --password=$TPZ_DB_USER_PASSWD -e "USE $TPZ_DB_NAME; SELECT 1 FROM zone_weather LIMIT 1"; do
     sleep 5
+    echo "Retrying connection to Database..."
 done
+
 sleep 5
 echo "starting topaz_connect"
 nohup ./topaz_connect > topaz_connect.log &
